b1dfd92774d17cd790039cc2e07afab3e263ba40
lmk <1475239605@qq.com>
2022-03-07 13:39:20 +0800

    first commit

---
+++ src/com/java/Test.java
@@ -0,0 +1,11 @@
+package com.java;
+
+/**
+ * @author lmk
+ * @create 2022-03-07 13:35
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
